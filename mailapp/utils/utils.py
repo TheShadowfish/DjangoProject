3cea409bb3bab5cff1a138b84443536b99b4a07b
Эдуард Кусков <shadowfish@yandex.ru>
2024-06-14 00:32:12 +0300

    redirect is working, settings is prepared to realize ++++++, send_mail. Ready for crontab.

--- mailapp/utils/utils.py
+++ mailapp/utils/utils.py
@@ -15,9 +15,8 @@ def get_info_and_send(mailing_item: Mailing):
     # print(f"mailing_item.message={mailing_item.message.title}")
     # print(f"mailing_item.message={mailing_item.message.body}")
 
-
-
-    message = Message.objects.get(mailing_item.message_id)
+    message = Message.objects.get(pk=mailing_item.message_id)
+    # message = Message.objects.get(mailing_item.message_id)
     print(f"message={message}...")
 
 
@@ -35,27 +34,27 @@ def get_info_and_send(mailing_item: Mailing):
     # mail_list = Client.objects.get(mailing=mailing_item)
 
     mail_list = Client.objects.filter(mailing=mailing_item)
-    print(f"mail_list={mail_list.email}...")
+    [print(f"mail_list={client.email}...") for client in mail_list]
 
-    for mail in mail_list:
+    for client in mail_list:
         result = ''
         try:
-            print(f"subject={mail_title}, message={mail_body}, from_email={mail_from}, "
-                  f"to_email={mail.email}")
+            print(f"subject={mail_title}, message={mail_body}, from_email={settings.EMAIL_HOST_USER}, "
+                  f"to_email={client.email}")
 
             result = send_mail(
                 subject=mail_title,
                 message=mail_body,
                 from_email=settings.EMAIL_HOST_USER,
-                recipient_list=[mail.email],
+                recipient_list=[client.email],
                 fail_silently=False,
             )
 
-            log_text = f'Success!, time={timezone.now()}, mailing={mailing_item.title}, mail={mail.email}'
+            log_text = f'Success!, time={timezone.now()}, mailing={mailing_item.title}, mail={client.email}'
             log = MailingLog.objects.create(log_text=log_text, mailing=mailing_item, status=True, mail_answer=result)
             log.save()
 
         except SMTPException as error:
-            log_text = f"Can't send: {error}, time={timezone.now()}, mailing={mailing_item.title}, mail={mail.email}"
+            log_text = f"Can't send: {error}, time={timezone.now()}, mailing={mailing_item.title}, mail={client.email}"
             log = MailingLog.objects.create(log_text=log_text, mailing=mailing_item, status=False, mail_answer=result)
             log.save()
